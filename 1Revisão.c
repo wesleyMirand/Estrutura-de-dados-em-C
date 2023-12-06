91495fc99d173a96cd43a2d163d31149e591be72
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-06 11:29:06 -0300

    Create 1Revisão.c

---
+++ 1Revisão.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int a = 5;
+float num = 5.5;
+double d = 4.6;
+char oi = 'i';
+
+int main(void) {
+  printf("o valor da var 'a' é: %d\n", a);
+
+  printf("o valor da var num é : %.1f\n", num);
+
+  printf("o valor da var d é: %.1f\n", d);
+
+  printf("o valor da var oi é: %c\n", oi);
+
+  return 0;
+}
