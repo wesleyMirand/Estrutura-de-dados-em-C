a578990a844ed90aa6cb7296a2677bd6d78c3ae8
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-06 21:27:11 -0300

    Create revisao2.c

---
+++ revisao2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//entrada de dados
+int main() {
+  int num, soma;
+
+  printf("\nDigite um numero:");
+  scanf("%d", &num);
+
+  soma = num + 5;
+
+  printf("O valor da var 'num' + 5 é : %d", soma);
+
+  return 0;
+}
