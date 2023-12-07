f9640e4b7546e08cc6ce1a4cd063bc8f76292152
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-06 21:37:41 -0300

    Create 21revisaoVetorArrays.c

---
+++ 21revisaoVetorArrays.c
@@ -0,0 +1,16 @@
+//Vetores e Arrays que são armazenadas sequencialmente. Os vetores ajudam a criar uma sequencia de caracteres.Cada posição eu armazeno elementos e adicinamos valores as variaveis.Armazenei 4elementos e cada indice "[2]" armazeno um elemento com valor. no int v[4] no começo eu declaro a quantidade de elemento, lembrando que sempre começa com o indice 0.
+
+#include <stdio.h>
+
+int v[4];
+
+int main()
+{
+  v [0] = 45;//armazenando valor na var
+  V [1] = 78;//indice 1 valor 78
+  V [2] = 9;
+  V [3] = 5;
+
+  
+  return 0;
+}
