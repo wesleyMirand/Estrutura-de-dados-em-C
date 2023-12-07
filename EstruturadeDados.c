881c1d4df52f0cb322775687049eb5d9121f791f
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-07 20:52:23 -0300

    Update EstruturadeDados.c

--- EstruturadeDados.c
+++ EstruturadeDados.c
@@ -1,4 +1,4 @@
-/* Alocação dinamica de estrutura de dados, para poder inicializar em tempo de execução.A diferenca em ter var estatica no incio do programa e sobre demanda durante o programa. Iniciar com um problema e dar continuidade nele com lista, pilhas... Iremos conseguir criar estrutura de dados mais elaboradas e alocadas dinamicamente. Alocar quantidade grande de estruturas para armazena pontos no espaço x e y. E criar um typedef pra chamar a struct de PONTO e por valores nele.
+/* Alocação dinamica de estrutura de dados, para poder inicializar em tempo de execução.A diferenca em ter var estatica no incio do programa e sobre demanda durante o programa. Iniciar com um problema e dar continuidade nele com lista, pilhas... Iremos conseguir criar estrutura de dados mais elaboradas e alocadas dinamicamente. Alocar quantidade grande de estruturas para armazena pontos no espaço x e y. E criar um typedef pra chamar a struct de PONTO e por valores nele.*/
 
 #include <stdio.h>
 
