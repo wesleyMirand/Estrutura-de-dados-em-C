63b337ab7f3ed4e8a90b35777a9ad75ec758737c
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-06 21:28:17 -0300

    Create 4revisao.c

---
+++ 4revisao.c
@@ -0,0 +1,14 @@
+
+//entrada de dados
+#include <stdio.h>
+
+int main() {
+  char letra;
+
+  printf("\nDigite uma letra:");
+  scanf("%c", &letra);
+
+  printf("a letra escolhida foi : %c\n", letra);
+
+  return 0;
+}
