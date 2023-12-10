2e0b3a0e145cd969a911607c22593148a07726d8
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-09 23:40:33 -0300

    Create structLIsta.c

---
+++ structLIsta.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+struct ponto {
+  float x;
+  float y;
+};
+
+typed struct ponto Ponto;
+
+int maisn(void) {
+  Ponto *p = (Ponto *)malloc(sizeof(Ponto));
+  p->1;
+  p->5 return;
+  return 0;
+}
