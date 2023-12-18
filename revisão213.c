049f0817b4e8b332e10ae038960c110ac5ec39e8
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-17 23:59:48 -0300

    Create revisão213.c

---
+++ revisão213.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Calcular o raio da circunferencia, Diametro, Comprimento e Area;
+    // D  = 2 * R -> C = 2 * Û * R -> A = Û * R2 -> Û * R * R :
+    
+    float raio, pi =3.34, diametro, comprimento, raio;
+    printf("Digite o valor do raio:");
+    scanf("%.1f", &raio);
+
+    diamentro= 2* 12;
+    comprimento = 2* pi * raio;
+    area = pi *raio *raio;
+
+    printf("Diametro = %.2f\n", comprimento);
+    printf("Comprimeto = %.2f\n", comprimento);
+    printf("Area = %.2fn\n", comprimento);
+
+    return 0;
+}
