eba1deeb64308111468534ffcaa2ee3760082ab4
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-24 21:06:42 -0300

    Create 2LojaCelularTec.c

---
+++ 2LojaCelularTec.c
@@ -0,0 +1,57 @@
+//removendo o item apple
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct celulartec {
+  char modelo; // qual o modelo do celular
+  struct celulartec *prox;
+};
+
+typedef struct celulartec Celulartec;
+
+Celulartec *bateria;
+
+void add(char modelo) {
+  Celulartec *p = (Celulartec *)malloc(sizeof(Celulartec));
+  p->modelo = modelo;
+  p->prox = bateria;
+  bateria = p;
+}
+
+void rem() {
+  if (bateria == NULL) {
+    printf("Bateria Vazia");
+  } else {
+    bateria = bateria->prox;
+  }
+}
+
+void imprime(Celulartec *p) {
+  if (p != NULL) {
+    printf("\nMarca do Celular: %c\n", p->modelo);
+    imprime(p->prox);
+  }
+}
+
+int main() {
+
+  add('e');
+  add('l');
+  add('l');
+  add('p');
+  add('a'); // o ultimo no topo da fila
+
+  rem();
+  rem();
+  rem();
+  rem();
+  rem();
+  rem();
+  
+
+  Celulartec *bateriaAux = bateria;
+  imprime(bateriaAux);
+
+  return 0;
+}
